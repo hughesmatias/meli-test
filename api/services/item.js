05f55e43fb9d61508b1c794766367461e3f6ecf8
Matias Perez Hughes <hughesmatias@gmail.com>
2020-11-11 00:13:26 -0300

    item service - capa de services para llamar a la api ext de Meli

---
+++ api/services/item.js
@@ -0,0 +1,14 @@
+
+class ItemService {
+  getItem(id) {
+    // call external service
+    return false;
+  };
+
+  search(query) {
+    // call external serfice
+    return false;
+  };
+};
+
+module.exports = new ItemService();
