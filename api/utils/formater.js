5c8e6fd247a16508af29e037591c2529802b4d47
Matias Perez Hughes <hughesmatias@gmail.com>
2020-11-15 23:39:44 -0300

    localidad del item buscado

--- api/utils/formater.js
+++ api/utils/formater.js
@@ -14,6 +14,7 @@ const getItemInformation = (item) => ({
   picture: item.thumbnail,
   condition: item.condition,
   free_shipping: item.shipping.free_shipping || false,
+  location: `${item.address ? item.address.city_name + ', ' + item.address.state_name : false}`,
 });
 
 module.exports = {
